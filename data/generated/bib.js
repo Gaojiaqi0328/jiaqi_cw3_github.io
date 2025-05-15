7efa7615c375c8479f7d9126e84b6250504e5fa5
Gaojiaqi0328 <psxjg14@nottingham.ac.uk>
2025-05-15 12:59:40 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,126 @@
+﻿const generatedBibEntries = {
+    "AlYakoob2017": {
+        "author": "S. Al-Yakoob and H.D. Sherali",
+        "doi": "10.15388/INFORMATICA.2017.147",
+        "journal": "Informatica",
+        "number": "4",
+        "pages": "739--756",
+        "title": "A Column Generation Mathematical Model for a Teaching Assistant Workload Assignment Problem",
+        "type": "article",
+        "volume": "28",
+        "year": "2017"
+    },
+    "Beck2016Visual": {
+        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively...",
+        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
+        "doi": "10.1109/TVCG.2015.2467757",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
+        "number": "01",
+        "pages": "",
+        "publisher": "IEEE",
+        "series": "TVCG",
+        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "type": "article",
+        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
+        "volume": "22",
+        "year": "2016"
+    },
+    "Caselli2020": {
+        "archiveprefix": "arXiv",
+        "author": "B. Caselli and M. Delorme and M. Iori",
+        "doi": "10.48550/arXiv.2005.09442",
+        "eprint": "2005.09442",
+        "journal": "arXiv Preprint",
+        "primaryclass": "cs.AI",
+        "title": "Integer Linear Programming for the Tutor Allocation Problem: A Practical Case in a British University",
+        "type": "article",
+        "year": "2020"
+    },
+    "Diallo2024": {
+        "author": "F.P. Diallo and C. Tudose",
+        "doi": "10.3390/app14209554",
+        "journal": "Applied Sciences",
+        "number": "20",
+        "pages": "9554",
+        "title": "Optimizing the Scheduling of Teaching Activities in a Faculty",
+        "type": "article",
+        "volume": "14",
+        "year": "2024"
+    },
+    "Kumar2020": {
+        "author": "A. Kumar and others",
+        "doi": "10.1007/s10951-020-00667-2",
+        "journal": "Journal of Scheduling",
+        "number": "6",
+        "pages": "735--750",
+        "title": "Balancing Workload and Expertise in TA Assignment: A Case Study of MIT\u2019s Computer Science Department",
+        "type": "article",
+        "volume": "23",
+        "year": "2020"
+    },
+    "Lee2022": {
+        "author": "H. Lee and S. Park",
+        "doi": "10.1109/TE.2022.3156789",
+        "journal": "IEEE Transactions on Education",
+        "number": "4",
+        "pages": "512--523",
+        "title": "A Genetic Algorithm for Joint Timetabling and Teaching Assistant Assignment in Large-scale Universities",
+        "type": "article",
+        "volume": "65",
+        "year": "2022"
+    },
+    "MayaDuque2012": {
+        "author": "P. Maya Duque and K. S\u00f6rensen and P. Goos",
+        "doi": "10.1016/j.cor.2011.04.001",
+        "journal": "Computers \\& Operations Research",
+        "number": "12",
+        "pages": "3213--3223",
+        "title": "A Metaheuristic for a Teaching Assistant Assignment-Routing Problem",
+        "type": "article",
+        "volume": "39",
+        "year": "2012"
+    },
+    "Smith2020": {
+        "author": "J. Smith and L. Wang",
+        "doi": "10.1016/j.ejor.2020.11.045",
+        "journal": "European Journal of Operational Research",
+        "number": "3",
+        "pages": "987--1002",
+        "title": "A Unified Model for Course Timetabling and Teaching Assistant Assignment in Universities",
+        "type": "article",
+        "volume": "292",
+        "year": "2020"
+    },
+    "Valouxis2021": {
+        "author": "C. Valouxis and others",
+        "doi": "10.1016/j.ejor.2021.12.012",
+        "journal": "European Journal of Operational Research",
+        "number": "1",
+        "pages": "45--67",
+        "title": "A Survey on Integrated Educational Resource Allocation: From Timetabling to Staff Scheduling",
+        "type": "article",
+        "volume": "301",
+        "year": "2021"
+    },
+    "Zhang2021": {
+        "author": "Y. Zhang and others",
+        "doi": "10.1016/j.asoc.2021.107845",
+        "journal": "Applied Soft Computing",
+        "pages": "107845",
+        "title": "Simulated Annealing with Adaptive Weights for Multi-objective University Scheduling",
+        "type": "article",
+        "volume": "112",
+        "year": "2021"
+    },
+    "Zou2018": {
+        "author": "F. Zou and D. Chen",
+        "doi": "10.1016/j.neucom.2018.06.076",
+        "journal": "Neurocomputing",
+        "pages": "1--15",
+        "title": "A Survey of Teaching\u2013Learning-Based Optimization",
+        "type": "article",
+        "volume": "300",
+        "year": "2018"
+    }
+};
